e3a35ebce7b6839349ef4c6c0a3f632b84b0519a
csokolove <94080789+csokolove@users.noreply.github.com>
2022-02-27 15:41:46 -0500

    part 3

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 use std::io; // obtain io rust module
+use std::cmp::Ordering; // obtain Ordering rust module
 use rand::Rng; // obtain Rng rust module
+
 
 fn main() { // create main function (program entry point)
     println!("Guess the number!"); // outputs "Guess the number!"
     
-    let secret_number = rand::thread_rng().gen_range(1..100); // declare variable "secret_number", and assign it a random value between 1, inclusive, and 100, exclusive.
+    let secret_number = rand::thread_rng().gen_range(1..101); // declare variable "secret_number", and assign it a random value between 1, inclusive, and 100, exclusive.
                                                   // this is a range expression (ex. start..end) where the lower bound is inclusive, however the upper bound is exclusive (ex. 1..100) will only return a number between 1 & 99
                                                   // ...to make both inclusive, change the syntax to "..=" (ex. 1..=100) will return a number between 1 & 100, inclusive
     println!("Please input your guess."); // outputs "Please input your guess."
@@ -15,9 +17,18 @@ fn main() { // create main function (program entry point)
         .read_line(&mut guess) // read the input, and store the value in the mutable variable "guess"
         .expect("Failed to read line"); // if program errors, "expect" will crash the program with the error "Failed to read line"
 
+    let guess: u32 = guess.trim().parse().expect("Please type a number!"); // eliminate whitespace in the value & see if the value inputted is a number (u32). "parse()" parses a string into a number
+
     println!("You guessed: {}", guess); // outputs "You guessed: ", followed by the value of "guess"
                         // similar to Python, {} serves as a placeholder and takes on the value of the positional parameter (ex. first {} in a string receives the value of the first value passed after the string)
+
+    match guess.cmp(&secret_number) { // compare 2 values (guess & secret_number)
+        Ordering::Less => println!("Too small!"), // if guess is less than secret_number, output "Too small!"
+        Ordering::Greater => println!("Too big!"), // if guess is greater than secret_number, output "Too big!"
+        Ordering::Equal => println!("You win!") // if guess is equal to secret_number, output "You win!"
+    }
 }
 
-// If the program is run and 4 is inputted by the user, the program should return:
+// If the program is run, the secret number is 24, and 4 is inputted by the user, the program should return:
 // "You guessed: 4"
+// "Too small!"
